673fec4d61353411c6c1068e90baf3b4623bef7c
Jim Holmes <32bitkid@gmail.com>
2010-06-17 14:49:02 -0400

    Adding some tests.

---
+++ nBinLib.Tests/BitReaders/BitReader64Test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using nBinLib.Reader;
+using NUnit.Framework;
+
+namespace nBinLib.Tests.BitReaders
+{
+    [TestFixture]
+    class BitReader64Test
+    {
+        private Stream _stream;
+        private IBitReader _br;
+
+        [TestCase(1, new byte[] { 1 }, Result = 0u)]
+        [TestCase(2, new byte[] { 1 }, Result = 0u)]
+        [TestCase(3, new byte[] { 1 }, Result = 0u)]
+        [TestCase(4, new byte[] { 1 }, Result = 0u)]
+        [TestCase(5, new byte[] { 1 }, Result = 0u)]
+        [TestCase(6, new byte[] { 1 }, Result = 0u)]
+        [TestCase(7, new byte[] { 1 }, Result = 0u)]
+        [TestCase(8, new byte[] { 1 }, Result = 1u)]
+
+        [TestCase(1, new byte[] { 128 }, Result = 1u)]
+        [TestCase(2, new byte[] { 128 }, Result = 2u)]
+        [TestCase(3, new byte[] { 128 }, Result = 4u)]
+        [TestCase(4, new byte[] { 128 }, Result = 8u)]
+        [TestCase(5, new byte[] { 128 }, Result = 16u)]
+        [TestCase(6, new byte[] { 128 }, Result = 32u)]
+        [TestCase(7, new byte[] { 128 }, Result = 64u)]
+        [TestCase(8, new byte[] { 128 }, Result = 128u)]
+
+        [TestCase(1, new byte[] { 255 }, Result = 1u)]
+        [TestCase(2, new byte[] { 255 }, Result = 3u)]
+        [TestCase(3, new byte[] { 255 }, Result = 7u)]
+        [TestCase(4, new byte[] { 255 }, Result = 15u)]
+        [TestCase(5, new byte[] { 255 }, Result = 31u)]
+        [TestCase(6, new byte[] { 255 }, Result = 63u)]
+        [TestCase(7, new byte[] { 255 }, Result = 127u)]
+        [TestCase(8, new byte[] { 255 }, Result = 255u)]
+
+        public uint ShouldReadAByte(int length, byte[] data)
+        {
+            _stream = new MemoryStream(data);
+            _br = new BitReader64(_stream);
+
+            uint actual;
+            _br.Peek(length, out actual);
+            return actual;
+        }
+    }
+}
---
+++ nBinLib.Tests/CircularBuffers/RingBufferedStreamTests.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using nBinLib.CircularBuffers;
+using NUnit.Framework;
+
+namespace nBinLib.Tests.CircularBuffers
+{
+    [TestFixture]
+    public class RingBufferedStreamTests
+    {
+        [Test]
+        public void ShouldPerformSimpleRead()
+        {
+            var rb = new RingBufferedStream(3);
+            rb.Write(new byte[] {1, 2, 3, 4, 5, 6, 7, 8}, 0, 8);
+            var reader = new byte[8];
+            rb.Read(reader, 0, 2);
+            Assert.AreEqual(new byte[] {1, 2, 0, 0, 0, 0, 0, 0}, reader);
+            rb.Read(reader, 2, 2);
+            Assert.AreEqual(new byte[] { 1, 2, 3, 4, 0, 0, 0, 0 }, reader);
+            rb.Read(reader, 4, 4);
+            Assert.AreEqual(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 }, reader);
+        }
+
+        [Test]
+        public void ShouldWrapAround()
+        {
+            var reader = new byte[8];
+            var rb = new RingBufferedStream(3);
+            
+            rb.Write(new byte[] { 1, 2, 3, 4 }, 0, 4);
+
+            rb.Read(reader, 0, 4);
+            Assert.AreEqual(new byte[] { 1, 2, 3, 4, 0, 0, 0, 0 }, reader);
+
+            rb.Write(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 0, 8);
+
+            rb.Read(reader, 0, 8);
+            Assert.AreEqual(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 }, reader);
+        }
+
+        [Test]
+        public void ShouldSeekForward()
+        {
+            var reader = new byte[8];
+            var rb = new RingBufferedStream(3);
+
+            rb.Write(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 0, 8);
+            rb.Seek(2, SeekOrigin.Current);
+            rb.Read(reader, 0, 1);
+            Assert.AreEqual(3, reader[0]);
+        }
+
+        [Test]
+        public void SeekShouldWrap()
+        {
+            var reader = new byte[8];
+            var rb = new RingBufferedStream(3);
+
+            rb.Write(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 0, 8);
+            rb.Seek(4, SeekOrigin.Current);
+            rb.Write(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 0, 4);
+            rb.Seek(6, SeekOrigin.Current);
+            rb.Read(reader, 0, 1);
+            Assert.AreEqual(3, reader[0]);
+        }
+    }
+}
